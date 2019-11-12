c79af09f43dee021ba0935a99e4f6a2791732123
kevinchamberlainICF <54120049+kevinchamberlainICF@users.noreply.github.com>
2019-11-12 12:29:24 -0500

    more on functions

--- chapters1to4.py
+++ chapters1to4.py
@@ -104,3 +104,11 @@ print_twice(4)
 print_twice(math.pi)
 print_twice('Spam '*4)
 print_twice('Span Spam Spam Spam')'''
+
+# using Return with void functions
+def addtwo(a, b):
+    added = a + b
+    return added
+
+x = addtwo(3,4)
+print(x)
