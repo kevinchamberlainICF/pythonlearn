25fc83527d708f00741237699f8526c31d6269ab
kevinchamberlainICF <54120049+kevinchamberlainICF@users.noreply.github.com>
2019-11-13 15:11:36 -0500

    test

---
+++ ch5.py
@@ -0,0 +1,29 @@
+import math
+
+#confirm correct script
+print("chapter 5")
+
+'''n = 5
+
+while n > 0:
+    print(n)
+    n = n - 1
+print("blast off!")'''
+
+# using break to stop an infinite loop
+'''while True:
+    line = input('> ')
+    if line == 'done':
+        break
+    print(line)
+print('Done!')'''
+
+#using continue in while loops
+'''while True:
+    line = input('> ')
+    if line[0] == '#':
+        continue
+    if line == 'done':
+        break
+    print(line)
+print('Done!')'''
